08d94c7c52bad231694cb9ae76c54eea2659d11a
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-03-26 10:42:24 +0100

    Rename input/grid pts/val

--- src/biweight.cpp
+++ src/biweight.cpp
@@ -15,22 +15,23 @@ Cbiweight(
   double radius,
   bool ind_normalize)
 {
-  int nr = grid_x.size();
-  int nc = input_val.ncol();
+  int nb_input_pts = input_x.size();
+  int nb_grid_pts = grid_x.size();
+  int nb_var = input_val.ncol();
 
-  NumericMatrix hexval(nr, nc);
+  NumericMatrix grid_val(nb_grid_pts, nb_var);
 
   double radius2 = radius * radius;
 
   #pragma omp parallel for schedule(static)
-  for (int i = 0; i < input_x.size(); ++i) {
+  for (int i = 0; i < nb_input_pts; ++i) {
     double d2;
     double pond;
     double sumpond = 0;
     std::vector< std::tuple<int, double> > ponds;
     ponds.reserve(1024);
 
-    for (int j = 0; j < nr; ++j) {
+    for (int j = 0; j < nb_grid_pts; ++j) {
       double x = input_x[i] - grid_x[j];
       double y = input_y[i] - grid_y[j];
 
@@ -52,21 +53,21 @@ Cbiweight(
           ponds.push_back(std::make_tuple(j, pond));
           sumpond += pond;
         } else {
-          for (int k = 0; k < nc; ++k) {
+          for (int k = 0; k < nb_var; ++k) {
             #pragma omp atomic update
-            hexval(j, k) += pond * input_val(i, k);
+            grid_val(j, k) += pond * input_val(i, k);
           }
         }
       }
     }
     if (ind_normalize && sumpond > 0) {
       for (auto [ j, jpond ] : ponds) {
-        for (int k = 0; k < nc; ++k) {
+        for (int k = 0; k < nb_var; ++k) {
           #pragma omp atomic update
-          hexval(j, k) += jpond * input_val(i, k) / sumpond;
+          grid_val(j, k) += jpond * input_val(i, k) / sumpond;
         }
       }
     }
   }
-  return hexval;
+  return grid_val;
 }
